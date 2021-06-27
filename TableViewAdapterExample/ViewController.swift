8f87a14cdceee24b96a4e26f74445e0aff061e72
dp221125 <dp221125@naver.com>
2021-06-28 00:59:18 +0900

    Add ViewModel

--- TableViewAdapterExample/ViewController.swift
+++ TableViewAdapterExample/ViewController.swift
@@ -9,29 +9,65 @@ import UIKit
 import RxSwift
 import RxCocoa
 
+protocol ViewBindable {
+    var cellSeleted: PublishRelay<String> { get }
+    
+    var seletedData: Signal<String> { get}
+    
+    var disposeBag: DisposeBag { get }
+}
+
+struct ViewModel: ViewBindable {
+    //input
+    let cellSeleted = PublishRelay<String>()
+    
+    //output
+    let seletedData: Signal<String>
+    
+    let disposeBag = DisposeBag()
+    
+    init() {
+        seletedData = cellSeleted
+            .asSignal(onErrorSignalWith: .empty())
+    }
+}
+
 class ViewController: UIViewController {
     
     lazy var mapper = ViewMapper()
+    
+    let disposeBag = DisposeBag()
+    let viewModel = ViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemBackground
         
-        let tableView = mapper.mapTableView()
+        let tableView = mapper.mapTableView(viewModel: viewModel)
         
         self.view.addSubview(tableView)
         
         tableView.snp.makeConstraints {
             $0.top.leading.trailing.bottom.bottom.equalTo(self.view.safeAreaLayoutGuide)
         }
-
+        
+        self.bind(viewModel: viewModel)
+    }
+    
+    func bind(viewModel: ViewBindable) {
+        
+        viewModel.seletedData
+            .emit(onNext: {
+                Swift.print($0)
+            })
+            .disposed(by: disposeBag)
+            
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         Swift.print("로드완료")
     }
-
 
 }
 
@@ -40,11 +76,17 @@ struct ViewMapper {
     lazy var render = TableViewRender(data: Job.allCases)
     lazy var adapter = TableViewAdapter(render: render)
     
-    mutating func mapTableView() -> UITableView {
+    mutating func mapTableView(viewModel: ViewBindable) -> UITableView {
         let tableView = UITableView()
         tableView.register(ExampleCell.self, forCellReuseIdentifier: String(describing: ExampleCell.self))
         tableView.dataSource = adapter
         tableView.delegate = adapter
+        
+        render.selected
+            .map { $0.displayName }
+            .bind(to: viewModel.cellSeleted)
+            .disposed(by: viewModel.disposeBag)
+        
         return tableView
     }
 }
